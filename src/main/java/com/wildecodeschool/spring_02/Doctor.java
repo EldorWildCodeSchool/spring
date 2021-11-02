161ae88a1971a70cd128a8d04739d27289f4a875
EldorWildCodeSchool <eldor-rabe_student2021@wilder.school>
2021-11-02 16:39:13 +0100

    Initial commit with the results for the "spring-2"-quest

---
+++ src/main/java/com/wildecodeschool/spring_02/Doctor.java
@@ -0,0 +1,36 @@
+package com.wildecodeschool.spring_02;
+
+public class Doctor {
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "actorID=" + actorID +
+                ", actorName='" + actorName + '\'' +
+                '}';
+    }
+
+    private int actorID;
+    private String actorName;
+
+    public Doctor(int actorID, String actorName) {
+        this.actorID = actorID;
+        this.actorName = actorName;
+    }
+
+    public int getActorID() {
+        return actorID;
+    }
+
+    public void setActorID(int actorID) {
+        this.actorID = actorID;
+    }
+
+    public String getActorName() {
+        return actorName;
+    }
+
+    public void setActorName(String actorName) {
+        this.actorName = actorName;
+    }
+}
---
+++ src/main/java/com/wildecodeschool/spring_02/Main2.java
@@ -0,0 +1,55 @@
+package com.wildecodeschool.spring_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@SpringBootApplication
+public class Main2 {
+
+    private static List<Doctor> myDoctors = new ArrayList<>();
+    public static void main(String[] args) {
+        SpringApplication.run(Main2.class, args);
+
+        Doctor doctor9 = new Doctor(9, "Christopher Eccleston");
+        Doctor doctor10 = new Doctor(10, "David Tennant");
+        Doctor doctor11 = new Doctor(11, "Matt Smith");
+        Doctor doctor12 = new Doctor(12, "Peter Capaldi");
+        Doctor doctor13 = new Doctor(13,"Jodie Whittaker");
+
+        myDoctors.add(doctor9);
+        myDoctors.add(doctor10);
+        myDoctors.add(doctor11);
+        myDoctors.add(doctor12);
+        myDoctors.add(doctor13);
+
+    }
+
+    @RequestMapping("/doctor/{id}")
+    @ResponseBody
+    public String hello(@PathVariable int id) {
+
+        if (id < 9) {
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER, "This doctor has retired! Please choose another one (maybe 9 - 13?)");
+        }
+        if (id > 13) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible to retrieve the incarnation " + id);
+        }
+        for (Doctor doctor : myDoctors) {
+            if (doctor.getActorID() == id) {
+                return doctor.toString();
+            }
+        }
+        return "";
+
+    }
+
+}
